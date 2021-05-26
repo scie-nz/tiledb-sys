40ace8458ce2399f6e0b0a1b49458cf23f3a4234
Bogdan State <bogdan@scie.nz>
2021-05-27 10:58:12 +1200

    Cleaned up build.rs

--- build.rs
+++ build.rs
@@ -4,21 +4,14 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
+    // Tell cargo to tell rustc to link the tiledb library.
     println!("cargo:rustc-link-lib=dylib=tiledb");
-    println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rustc-link-lib=crypto");
-    println!("cargo:rustc-link-lib=zstd");
-    println!("cargo:rustc-link-lib=lz4");
-    println!("cargo:rustc-link-lib=z");
-    println!("cargo:rustc-link-lib=bz2");
-    println!("cargo:rustc-flags=-ltiledb");
-
+    // search for the library in a custom location
     println!("cargo:rustc-link-search=/home/bogdan/TileDB/dist/lib");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.hpp");
+
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
@@ -26,15 +19,17 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.hpp")
-        .rustfmt_bindings(true)
+        .rustified_enum(".*")
+        .generate_inline_functions(true)
         .clang_args(vec![
             "-I/home/bogdan/TileDB/dist/include",
             "-std=c++11",
         ])
-        .size_t_is_usize(false)
         .enable_cxx_namespaces()
         .opaque_type("std::.*")
-        //.opaque_type("tiledb::VFSFilebuf.*")
+        .blocklist_type("int_type")
+        .blocklist_type("off_type")
+        .allowlist_type("tiledb.*")
         .module_raw_lines(
             "root",
             [
@@ -44,23 +39,8 @@ fn main() {
             .iter()
             .map(|s| *s),
         )
-        .opaque_type("tiledb::impl_::ConfigIter")
-        .opaque_type("tiledb::ObjectIter_iterator")
-        .blocklist_type("int_type")
-        .blocklist_type("off_type")
-        .ignore_methods()
-        .opaque_type(".*VFSFilebuf.*")
-        .allowlist_type("tiledb.*")
-        .blocklist_type("tiledb::VFSFilebuf.*")
-        .opaque_type(".*VFS_filebuf.*")
-        .blocklist_type(".*ObjectIter_iterator_iterator1.*")
-        .generate_inline_functions(true)
-        .rustified_enum(".*")
-        .new_type_alias(".*")
         .derive_copy(false)
         .allowlist_function("tiledb.*")
-        .allowlist_var("tiledb.*")
-        .allowlist_function("tiledb::Context.*")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
